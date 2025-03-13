c0ee25990c3f118ea70b646d124c45acb96a830a
Guilherme-Freitas8022 <142058672+Guilherme-Freitas8022@users.noreply.github.com>
2025-03-12 23:19:02 -0300

    Update SomaDoisNumeros.cs
    tarefa finalizada

--- FixacaoConceitosBasicos1/Model/SomaDoisNumeros.cs
+++ FixacaoConceitosBasicos1/Model/SomaDoisNumeros.cs
@@ -8,9 +8,8 @@ namespace FixacaoConceitosBasicos1.Model
 {
     public class SomaDoisNumeros
     {
-        public static int Somar(int num1,  int num2)
+        public static int Somar(int num1, int num2)
         {
             return num1 + num2;
         }
     }
-}
